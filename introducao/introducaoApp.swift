c04d05d956a4b1ffd1dbec9c823674b627b9150e
angelilton <epifanio.rds@gmail.com>
2024-01-01 21:02:28 -0400

    Initial Commit

---
+++ introducao/introducaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  introducaoApp.swift
+//  introducao
+//
+//  Created by mac on 01/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct introducaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
