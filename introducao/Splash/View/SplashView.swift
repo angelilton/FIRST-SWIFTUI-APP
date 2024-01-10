1c36b7435672f508f3fb08b1e9307c2bf8b91852
angelilton <epifanio.rds@gmail.com>
2024-01-09 22:01:47 -0400

    add splash screen end logo

--- introducao/Splash/View/SplashView.swift
+++ introducao/Splash/View/SplashView.swift
@@ -9,24 +9,62 @@ import SwiftUI
 
 struct SplashView: View {
     
-    @State var ScreenState: SplashUIState = .loading
+    @State var ScreenState: SplashUIState = .error("erro ao carregar")
     
     var body: some View {
         switch ScreenState {
         case .loading:
-            Text("Loading")
+            loadingView
         case .goToSignInScreen:
             Text("SignIn Screen")
         case .goToHomeScreen:
             Text("Home Screen")
         case .error(let msg):
-            Text("Home Screen \(msg)")
+            errorView(m: msg)
         }
     }
 }
 
+// struct para reutilizar
+//struct LoadingView: View {
+//    var body: some View {
+//        ZStack {
+//            Image("logo")
+//                .resizable()
+//                .scaledToFit()
+//                .frame(maxWidth: .infinity, maxHeight: .infinity)
+//                .background(Color.green)
+//                .ignoresSafeArea()
+//        }
+//    }
+//}
+
+//e um extensao so e usado aqui
+extension SplashView {
+    var loadingView: some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .background(Color.green)
+                .ignoresSafeArea()
+        }
+    }
+}
+
+extension SplashView {
+    func errorView (m: String) -> some View {
+        ZStack {
+            Text("Home Screen \(m)")
+        }
+    }
+}
+
+
+
 struct SplashView_Previews: PreviewProvider {
     static var previews: some View {
-        SplashView(ScreenState: .error("desculpa deu erro"))
+        SplashView()
     }
 }
