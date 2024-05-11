3da75809b9bb5d2987faafba7f8b86a474162947
angelilton <epifanio.rds@gmail.com>
2024-05-11 16:26:04 -0400

    add editable fields and navigation to select gender

---
+++ introducao/Profile/View/GenderSelectorView.swift
@@ -0,0 +1,45 @@
+//
+//  GenderSelectorView.swift
+//  introducao
+//
+//  Created by mac on 11/05/2024.
+//
+
+import SwiftUI
+
+struct GenderSelectorView: View {
+    let title: String
+    let genders:[Gender]
+    
+    @Binding var selectedGender: Gender?
+    
+    var body: some View {
+        Form {
+            Section(header: Text(title)) {
+                List(genders, id: \.id) { item in
+                    HStack {
+                        Text(item.rawValue)
+                        Spacer()
+                        Image(systemName:"checkmark")
+                            .foregroundColor(selectedGender == item ? .orange : .white)
+                    }
+                    .contentShape(Rectangle())
+                    .onTapGesture {
+                        if !(selectedGender == item) {
+                            selectedGender = item
+                        }
+                    }
+                    
+                }
+            }
+
+        }
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct GenderSelectorView_Previews: PreviewProvider {
+    static var previews: some View {
+        GenderSelectorView(title: "Sexo!", genders: Gender.allCases, selectedGender: .constant(.male))
+    }
+}
--- introducao/Profile/View/ProfileView.swift
+++ introducao/Profile/View/ProfileView.swift
@@ -8,8 +8,76 @@
 import SwiftUI
 
 struct ProfileView: View {
+    @State var fullName = ""
+      @State var email = "tiago.aguiar@teste.com.br"
+      @State var cpf = "111.222.333-11"
+      @State var phone = "(11) 1234-1234"
+      @State var birthday = "20/02/1990"
+      @State var selectedGender: Gender? = .male
+    
     var body: some View {
-        Text("profile view")
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("Dados cadastrais")) {
+                        HStack {
+                            Text("Nome")
+                            Spacer()
+                            TextField("Digite seu nome", text: $fullName)
+                                .keyboardType(.alphabet)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+                        HStack {
+                            Text("E-mail")
+                            Spacer()
+                            TextField("", text: $email)
+                                .disabled(true)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+                        HStack {
+                            Text("CPF")
+                            Spacer()
+                            TextField("", text: $cpf)
+                                .disabled(true)
+                                .foregroundColor(.gray)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+                        HStack {
+                            Text("Celular")
+                            Spacer()
+                            TextField("Digite seu numero", text: $phone)
+                                .keyboardType(.numberPad)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+                        HStack {
+                            Text("Data de nascimento")
+                            Spacer()
+                            TextField("Digite seu data nasc", text: $birthday)
+                                .multilineTextAlignment(.trailing)
+                        }
+                        
+                        NavigationLink (
+                            destination: GenderSelectorView(
+                                    title: "Escolha o gênero",
+                                    genders: Gender.allCases,
+                                    selectedGender: $selectedGender
+                                ),
+                            label: {
+                                Text("Gênero")
+                                Spacer()
+                                Text(selectedGender?.rawValue ?? "")
+                            }
+                       )
+                    }
+                }
+            }
+            .navigationBarTitle(Text("Editar Perfil"), displayMode: .automatic)
+        }
     }
 }
 
